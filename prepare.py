e2cda2ac9037129e4cfb9eced361764ef3785b9c
pranav1699 <pranavsadagopan@gmail.com>
2022-10-14 16:05:37 +0530

    Stages added

---
+++ prepare.py
@@ -0,0 +1,6 @@
+text="Hello world"
+
+with open("sample.txt","w") as sample:
+    sample.write(text)
+
+print("Prepare task is completed..!!")
---
+++ read.py
@@ -0,0 +1,5 @@
+with open("sample.txt","r") as sample:
+    sample.read()
+
+
+print("Read task is completed. !!")
